7e779f67a12d38284587267e9036ddcfbc0c97ed
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2023-03-10 19:34:09 +0300

    вычисляем сумму случайных чисел от 1 до 10

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,8 +1,6 @@
-﻿
-// суммируем два числа;
-int numberA = new Random().Next(1, 10);
+﻿int numberA = new Random().Next(1, 10); // случайное число от 1 до 10
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10); // случайное число от 1 до 10
 Console.WriteLine(numberB);
-int result = numberA = numberB;
- Console.WriteLine(result);
+int result = numberA + numberB;
+Console.WriteLine(result);
